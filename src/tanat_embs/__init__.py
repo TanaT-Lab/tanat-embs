d3ce3464073ca92f66b80f5cac1e870ffcea106a
Arnaud Duvermy <arnaud.duvermy@inria.fr>
2026-06-11 13:59:18 +0200

    feat: check latest version in __init__.py

--- src/tanat_embs/__init__.py
+++ src/tanat_embs/__init__.py
@@ -3,3 +3,8 @@
 TanaT-EMBS - TanaT Embeddings Library
 """
 
+from tanat_utils import check_latest_version
+
+# Check for updates
+# Emit a warning if a new version is available on PyPI
+check_latest_version("tanat-embs")
